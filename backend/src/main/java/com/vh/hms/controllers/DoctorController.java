720b786ebf2a48f21a905b96aa93a19e77077196
MattSaito <matheussaitofs@gmail.com>
2024-08-28 17:20:06 -0300

    Criado controlador para agendamentos
    tambem foi orgazinado melhor as classes de controle e servicos
    e por isso houve alterações nos caminhos dos arquivos

--- backend/src/main/java/com/vh/hms/controllers/DoctorController.java
+++ backend/src/main/java/com/vh/hms/controllers/DoctorController.java
@@ -1,8 +1,9 @@
 package com.vh.hms.controllers;
 
-import com.vh.hms.auth.AuthService;
+import com.vh.hms.services.AuthService;
 import com.vh.hms.domain.doctor.DoctorRequestDTO;
 import com.vh.hms.domain.doctor.DoctorResponseDTO;
+import com.vh.hms.services.AuthService;
 import com.vh.hms.services.DoctorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
