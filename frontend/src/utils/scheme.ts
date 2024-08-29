27057ba955c503d259c04628c96070d6dc57c925
Fernando <fedelbone@gmail.com>
2024-08-28 21:40:14 -0300

    [Frontend] Ajustes tela agendamento

--- frontend/src/utils/scheme.ts
+++ frontend/src/utils/scheme.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
 import { isValidCPF, isValidPhoneNumber } from "./validations";
-import Select from "react-select";
+
+export const appointmentSchema = z.object({
+  specialization: z.string(),
+  doctor: z.string(),
+  date: z.date(),
+  time: z.string(),
+});
 
 export const signUpSchema = z.object({
   firstName: z
@@ -11,34 +17,33 @@ export const signUpSchema = z.object({
     .min(2, { message: "O sobrenome deve conter ao menos 2 caracteres" }),
   email: z.string().email({ message: "Email inválido" }),
   cpf: z.string().refine((cpf) => isValidCPF(cpf), { message: "CPF inválido" }),
-  phone: z.string().refine((phoneNumber) => isValidPhoneNumber(phoneNumber), {
-    message: "Número de telefone inválido.",
-  }),
-  password: z
+  phone: z
     .string()
-    .min(5, { message: "A senha precisa conter ao menos 5 caracteres" }),
+    .refine((phoneNumber) => isValidPhoneNumber(phoneNumber), {
+      message: "Número de telefone inválido.",
+    }),
+    password: z.string().min(5, {message: "A senha precisa conter ao menos 5 caracteres"})
 });
 
 export const signInSchema = z.object({
-  login: z.string().email({ message: "Email inválido" }),
-  password: z
-    .string()
-    .min(5, { message: "A senha precisa conter ao menos 5 caracteres" }),
-});
+    login: z.string().email({ message: "Email inválido" }),
+    password: z.string().min(5, {message: "A senha precisa conter ao menos 5 caracteres"})
+})
 
 export const contactSchema = z.object({
-  sender: z
+    sender: z.string().min(2, { message: "O nome deve conter ao menos 2 caracteres"}),
+    email: z.string().email({message: "Email inválido"}),
+    phone: z
     .string()
-    .min(2, { message: "O nome deve conter ao menos 2 caracteres" }),
-  email: z.string().email({ message: "Email inválido" }),
-  phone: z.string().refine((phoneNumber) => isValidPhoneNumber(phoneNumber), {
-    message: "Número de telefone inválido.",
-  }),
-  message: z
-    .string()
-    .min(20, { message: "A mensagem deve conter no mínimo 20 caracteres" }),
+    .refine((phoneNumber) => isValidPhoneNumber(phoneNumber), {
+      message: "Número de telefone inválido.",
+    }),
+    message: z.string().min(20, {message: "A mensagem deve conter no mínimo 20 caracteres"})
 });
+
+
 
 export type SignUpSchema = z.infer<typeof signUpSchema>;
 export type SignInSchema = z.infer<typeof signInSchema>;
 export type ContactSchema = z.infer<typeof contactSchema>;
+export type AppointmentSchema = z.infer<typeof appointmentSchema>
