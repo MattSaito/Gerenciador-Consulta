8fbcb878de25d063cd34f0049c602962df8afafb
MattSaito <matheussaitofs@gmail.com>
2024-08-31 04:23:07 -0300

    [Refactor] Algumas refatorações foram feitas
    O MessageDTO foi refatorado para ser mais genérico e ter uma melhor robustez
    alguns arquivos foram alterados para trabalharem com o novo DetailedAppointmentResponseDTO

--- backend/src/main/java/com/vh/hms/domain/message/MessageDTO.java
+++ backend/src/main/java/com/vh/hms/domain/message/MessageDTO.java
@@ -2,6 +2,8 @@ package com.vh.hms.domain.message;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record MessageDTO(@NotBlank String sender, @NotBlank String email, @NotBlank String phone, @NotBlank String description) {
-    public MessageDTO (Message message) { this(message.getSender(), message.getEmail(), message.getPhone(), message.getDescription());}
+import java.util.UUID;
+
+public record MessageDTO(UUID id, @NotBlank String sender, @NotBlank String email, @NotBlank String phone, @NotBlank String description) {
+    public MessageDTO (Message message) { this(message.getMessageUUID(), message.getSender(), message.getEmail(), message.getPhone(), message.getDescription());}
 }
